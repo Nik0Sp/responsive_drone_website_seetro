dbd4b525f3eb9d2dece71434abfa34a543f0fead
Nik0Sp <nikopringer@gmail.com>
2023-08-13 14:58:32 +0200

    media queries update

--- main.js
+++ main.js
@@ -6,6 +6,27 @@ window.addEventListener("scroll",()=>{
 })
 
 
+//show/hide navbar menu
+const menu = document.querySelector(".navbar");
+const menuBtn = document.querySelector("#open-nav");
+const closeBtn = document.querySelector("#close-nav");
+
+menuBtn.addEventListener("click",()=>{
+    menu.style.display = "flex";
+    closeBtn.style.display ="inline-block";
+    menuBtn.style.display = "none";
+})
+
+//close navbar
+
+const closeNav = () =>{
+    menu.style.display = "none";
+    closeBtn.style.display="none";
+    menuBtn.style.display="inline-block";
+}
+
+closeBtn.addEventListener("click",closeNav)
+
 
 
 //==========NAVBAR SECTION END=============
